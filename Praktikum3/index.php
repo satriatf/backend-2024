634b2fd10e1c04ffa4465328061317c99efe4b34
SATRIA TRI FERDIANSYAH <143409278+satriatf@users.noreply.github.com>
2024-10-18 14:28:31 +0700

    Delete Praktikum3 directory

--- Praktikum3/index.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once "construct.php";
-$satria = new Person("Satria ", "Bandung ", "Teknik Informatika <br>");
-$ferdiansyah = new Person("Ferdiansyah ", "Jakarta ", "Teknik Informatika ");
-
-$satria->cetak();
-$ferdiansyah->cetak();
